dbaf058ce90d392c6d467f708c1ebcf0e81b4013
VietHoang <hoang.tv@haposoft.com>
2020-07-27 21:08:09 +0700

    Add js button close message

--- js/buttonmes.js
+++ js/buttonmes.js
@@ -4,3 +4,16 @@
     })
 }) 
 
+function closeMes(id) {
+    var closeBoxchat = document.getElementById(id);
+    closeBoxchat.style.display = "none";
+}
+
+function showMes(id) {
+    var boxchat = document.getElementById(id);
+    if(boxchat.style.display == "block") {
+        boxchat.style.display = "none";
+    } else {
+        boxchat.style.display = "block";
+    }
+}
